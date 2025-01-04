1021ff191dd2a928e6c33ea2eec51081d9ca8ef3
Sara Rafi <sararafi102@gmail.com>
2025-01-04 13:52:16 +0500

    my 90th commit after adding more product images

--- src/sara/sprettypicks/sara/sprettypicks/SaraSPrettyPicks.java
+++ src/sara/sprettypicks/sara/sprettypicks/SaraSPrettyPicks.java
@@ -14,6 +14,8 @@ import java.util.logging.Logger;
  */
 public class SaraSPrettyPicks {
     //create testcases and multi threading 
+    //testcases(4)
+    //multi threading (7)
    //we got it back
     //remember to first update info then put anything in cart etc
    
